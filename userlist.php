65288d4428e22301fe098c6bf19f2c101d895840
Tobias <tir@bang-olufsen.dk>
2018-02-14 09:42:02 +0100

    Delete images in userlist feature added

--- userlist.php
+++ userlist.php
@@ -2,11 +2,10 @@
 //On page load
 $conn = mysqli_connect("localhost", "root", "", "DatabaseExam");
 
-
 //delete variable is set
-if(isset($_GET['delete']))
+if(isset($_GET['deleteuser']))
 {
-    $deleteUserId = $_GET['delete'];
+    $deleteUserId = $_GET['deleteuser'];
     $deleteStatement = "DELETE FROM users WHERE id =".$deleteUserId;
     $deleteImages = "DELETE FROM images WHERE owner = $deleteUserId";
     if($conn->query($deleteImages) == TRUE){
@@ -23,8 +22,26 @@ if(isset($_GET['delete']))
     }
 }
 
+//delete images from database
+if(isset($_GET['deleteImage']))
+{
+    $imageId= $_GET['deleteImage'];
+    $deleteImageById= $deleteImages = "DELETE FROM images WHERE id = $imageId";
+    if($conn->query($deleteImageById))
+    {
+        echo "Image deleted";
+    }
+    else 
+    {
+        $conn->error;
+    }
+    
+}
+
 $sql = "SELECT id, username, name FROM users WHERE 1";
+$sqlImages = "SELECT * FROM images;";
 $result = $conn->query($sql);
+$resultImage = $conn->query($sqlImages);
 ?>
 
 <!DOCTYPE html>
@@ -50,6 +67,27 @@ $result = $conn->query($sql);
                 echo "<td>".$row['username']."</td>";
                 echo "<td>".$row['name']."</td>";
                 echo "<td><a href='?delete=".$row["id"]."'>Delete</a></td>";
+                echo "</tr>";
+            }
+            ?>
+        </table>
+        
+        <h1>Images:</h1>
+        <table style="width:100%; border: 1px solid black;text-align:center;">
+            <tr>
+                <th>id</th>
+                <th>Image</th>
+                <th>Owner</th>
+                <th>Delete</th>
+            </tr>
+
+            <?php 
+            while($row = $resultImage->fetch_assoc()){
+                echo "<tr>";
+                echo "<td>".$row["id"]."</td>";
+                echo "<td><img width='200' height='200' src='".$row['imageURL']."'></td>";
+                echo "<td>".$row['owner']."</td>";
+                echo "<td><a href='?deleteImage=".$row["id"]."'>Delete</a></td>";
                 echo "</tr>";
             }
             ?>
