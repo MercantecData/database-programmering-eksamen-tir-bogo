f52fb52c571d88408841a8669f5a36eac6a15865
Tobias <tir@bang-olufsen.dk>
2018-02-14 08:36:17 +0100

    Fixed userlist

--- userlist.php
+++ userlist.php
@@ -1,22 +1,35 @@
 <?php
-	$conn = mysqli_connect("localhost", "root", "", "DatabaseExam");
-	$sql = "SELECT id FROM users WHERE 1";
-	$result = $conn->query($sql);
+$conn = mysqli_connect("localhost", "root", "", "DatabaseExam");
+$sql = "SELECT id, username, name FROM users WHERE 1";
+$result = $conn->query($sql);
 ?>
 
 <!DOCTYPE html>
 <html>
-<head>
-	<title>User List</title>
+    <head>
+        <title>User List</title>
 
-</head>
-<body>
-	<h1>Users:</h1>
-	<?php 
-	while($row = $result->fetch_assoc()){
-		echo $row["name"];
-		echo "   <a href='?'>delete</a>";
-	}
-	?>
-</body>
+    </head>
+    <body>
+        <h1>Users:</h1>
+        <table style="width:100%; border: 1px solid black;text-align:center;">
+            <tr>
+                <th>id</th>
+                <th>Username</th>
+                <th>Name</th>
+                <th>Delete</th>
+            </tr>
+            
+            <?php 
+            while($row = $result->fetch_assoc()){
+                echo "<tr>";
+                echo "<td>".$row["id"]."</td>";
+                echo "<td>".$row['username']."</td>";
+                echo "<td>".$row['name']."</td>";
+                echo "<td><a href='?'>Delete</a></td>";
+                echo "</tr>";
+            }
+            ?>
+        </table>
+    </body>
 </html>
