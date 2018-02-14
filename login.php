0f6134f87cd57c5b9100d829781f787b11e06abd
Tobias <tir@bang-olufsen.dk>
2018-02-14 11:02:59 +0100

    Encryption password normal login working

--- login.php
+++ login.php
@@ -1,17 +1,23 @@
 <?php
+require('connect.php');
 session_start();
 $usrname = $_POST["username"];
 $password = $_POST["password"];
 
-$conn = mysqli_connect("localhost", "root", "", "DatabaseExam");
 
-$sql = "SELECT id, name FROM users WHERE username = '$usrname' AND password = '$password'";
-echo $sql . "<br>";
+$sql = "SELECT id,username, password, name FROM users WHERE username = '$usrname'";
+
 $result = $conn->query($sql);
-$row = $result->fetch_assoc();
-var_dump($row);
-$id = $row["id"];
-$name = $row["name"];
-$_SESSION['userID'] = $id;
-$_SESSION["userName"] = $name;
+
+while($row = $result->fetch_assoc()){
+    if(password_verify($password, $row['password']))
+    {
+        $id = $row["id"];
+        $name = $row["name"];
+        $_SESSION['userID'] = $id;
+        $_SESSION["userName"] = $name;
+    }
+}
+
 header("Location: index.php");//redirects back
+?>
